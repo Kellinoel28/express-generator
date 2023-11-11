a5f3972348edc073e1deaa17d8d6741804943967
Kelli Kennedy <kellinoel28@gmail.com>
2023-11-11 11:00:47 -0600

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1298097684406609',
+        clientSecret: '1d824d9233d63b54229d6156fbaa0c6a'
+    }
 }
